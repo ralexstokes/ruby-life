be40a40acb8aecd8c6907e3ce0bb3a9cbeda9bc2
Alex Stokes <r.alex.stokes@gmail.com>
2014-01-11 01:31:58 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Conway's Game of Life, in Ruby
+
+#### github.com/ralexstokes
+
+### Usage
+
+    ruby run.rb $INPUT_FILE
+
+where $INPUT_FILE is a JSON file with a group of rows.
+Each row is an array of strings {'dead', 'alive'} that is used to
+populate the program's representation of the game.
+
+For more information on the game itself, see here:
+[http://en.wikipedia.org/wiki/Conway's_Game_of_Life](http://en.wikipedia.org/wiki/Conway's_Game_of_Life)
+
+### Questions, comments?
+@ralexstokes on Twitter
---
+++ run.rb
@@ -0,0 +1,29 @@
+require './lib/life/'
+
+SEED_FILE = ARGV[0]
+
+unless SEED_FILE
+  puts "\n\tWelcome to #{MODULE_NAME}!"
+  puts "\tUsage: `ruby run.rb $INPUT_FILE`"
+  puts "\t=>  Generate a seed configuration $INPUT_FILE with `ruby lib/generate_game.rb`"
+  puts "\t=>  Example seed files are in ./examples/"
+  exit
+end
+
+def save_slug
+  ARGV[0].split('/')[1].split('.')[0]
+end
+
+grid = Grid.new SEED_FILE
+# grid.pad_boundaries!
+game = GameOfLife.new grid
+
+system 'clear'
+100.times do
+  game.advance
+  sleep 1
+  system 'clear'
+end
+
+SAVE_FILE = "./#{ARGV[0].split('/')[0]}/#{save_slug}.save.json"
+grid.save SAVE_FILE
